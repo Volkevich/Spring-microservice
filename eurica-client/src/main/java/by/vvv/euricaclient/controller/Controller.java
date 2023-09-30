58761e2225a7890865ea774c240ebaa17d263c46
Vitaly Volkevich <87798074+Volkevich@users.noreply.github.com>
2023-09-30 16:49:41 +0300

    commit

--- eurica-client/src/main/java/by/vvv/euricaclient/controller/Controller.java
+++ eurica-client/src/main/java/by/vvv/euricaclient/controller/Controller.java
@@ -1,5 +1,6 @@
 package by.vvv.euricaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/main")
 public class Controller {
+    @Value("${eureka.instance.instance-id}")
+    public String id;
     @GetMapping("/test")
     public String getText(){
-        return "Hello from API-Gateway!";
+        return id;
     }
 }
